ef1dd819c2fb2e81cb24b9c695234fc311944c9a
silverstring <brandon@papsidero.com>
2016-03-11 13:51:24 -0800

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will take a matrix and cache the inverse of the matrix.
+## cacheSolve converts the matrix that it gets from makeCacheMatrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix will take a matrix and cache the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  # establish inv as NULL
+  inv = NULL
+  
+  set = function(y){
+    # set x to be y outside the environment, and set inv to be NULL as well
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get = function() {
+    # get will return x from the set function
+    x
+  }
+  
+  setinverse = function(inverse){
+    # make inv return from inverse outside the environment
+    inv <<- inverse
+  }
+  
+  getinverse = function(){
+    # return inv from the setinverse function
+    inv
+  }
+  
+  # this list is returned
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve converts the matrix that it gets from makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # inv uses getinv on x
+  inv = x$getinv()
+  
+  # if the inverted matrix already exists, just use cache!
+  if(!is.null(inv)){
+    message("Getting Cached Data!")
+    return(inv)
+  }
+  
+  # use solve to invert matrix
+  mat.data = x$get()
+  inv = solve(mat.data,...)
+  
+  # use setinv on x
+  x$setinv(inv)
+  
+  # return our inverted matrix
+  return(inv)
+}
+
+## the flipper function executes the above and displays the result
+flipper = function(){
+  #my matrix is an invertible matrix that we will "flip"
+  r = rnorm(1000000)
+  mymatrix = matrix(r, nrow=2, ncol=2)
+  
+  print(mymatrix)
+  
+  # put matrix in make and use cacheSolve on it!
+  make = makeCacheMatrix(mymatrix)
+  cacheSolve(make)
 }
